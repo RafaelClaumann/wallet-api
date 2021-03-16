5870fa3807d38e477941ae8b5be3a54fdf6412dc
rafael claumann bernardes <rafael_claumann@hotmail.com>
2021-03-15 21:58:50 -0300

    arquivo schema do banco de dados

---
+++ schema.sql
@@ -0,0 +1,79 @@
+--Client (id, nome, cpf,  telefone)
+-- Wallet (id, description, balance, client_id)
+--      client_id references Client(id)
+-- Expense (id, description, value, state, type, wallet_id)
+--      wallet_id references Wallet(id)
+-- Expense_state (id, state)
+-- Expense_type (id, type)
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON CLIENT
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON WALLET
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON CLIENT
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TABLE CLIENT (
+	id INTEGER NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	cpf VARCHAR(15) NOT NULL,
+	telefone VARCHAR(15) NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	UNIQUE(cpf),
+	CONSTRAINT pk_client PRIMARY KEY (id)
+);
+
+CREATE TABLE WALLET (
+	id INTEGER NOT NULL,
+	description VARCHAR(140) DEFAULT('no description set'),
+	client_id INTEGER NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	CONSTRAINT pk_wallet PRIMARY KEY (id)
+	CONSTRAINT wallet_client FOREIGN KEY (client_id) REFERENCES CLIENT (id)
+);
+
+CREATE TABLE EXPENSE (
+	id INTEGER NOT NULL,
+	description VARCHAR(140) DEFAULT('no description set'),
+	value NUMERIC NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE EXPENSE_TYPE (
+	id SMALLINT NOT NULL,
+	type VARCHAR(40) NOT NULL,
+	CONSTRAINT pk_expense_type PRIMARY KEY (id)
+);
+
+CREATE TABLE EXPENSE_STATE (
+	id SMALLINT NOT NULL,
+	state VARCHAR(40) NOT NULL,
+	CONSTRAINT pk_expense_state PRIMARY KEY (id)
+);
+
+INSERT INTO public.expense_state(id, state) VALUES (1, 'OPEN');
+INSERT INTO public.expense_state(id, state) VALUES (2, 'CLOSED');
+
+INSERT INTO public.expense_type(id, type) VALUES (1, 'HOUSE');
+INSERT INTO public.expense_type(id, type) VALUES (2, 'CAR');
+INSERT INTO public.expense_type(id, type) VALUES (3, 'WATER');
+INSERT INTO public.expense_type(id, type) VALUES (4, 'OTHER');
